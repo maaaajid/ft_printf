ed9fd1ebd3b1d5eadfd9f5f976e105d310c429d1
Abdelmajid Elbouaami <aelbouaa@e2r12p10.1337.ma>
2023-05-15 02:34:02 +0100

    ft_printf

---
+++ putptr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   putptr.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelbouaa <aelbouaa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/12 13:30:58 by aelbouaa          #+#    #+#             */
+/*   Updated: 2022/11/13 23:29:43 by aelbouaa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"ft_printf.h"
+
+int	putptr(unsigned long long num)
+{
+	int		x;
+	char	*str;
+
+	str = "0123456789abcdef";
+	x = 0;
+	
+	if (num > 15)
+	{
+		x = x + putptr(num / 16);
+		x = x + putptr(num % 16);
+	}
+	else
+		x = x + ft_putchar(str[num]);
+	return (x);
+}
